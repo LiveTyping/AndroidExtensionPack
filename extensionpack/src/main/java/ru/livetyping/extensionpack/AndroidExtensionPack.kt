cf36a3c71f5f6df333572ab79d75b4c2dd6405ec
DenisPopkov <d.popkov@ltst.ru>
2022-09-07 14:53:17 +0600

    added coroutines extensions

--- extensionpack/src/main/java/ru/livetyping/extensionpack/AndroidExtensionPack.kt
+++ extensionpack/src/main/java/ru/livetyping/extensionpack/AndroidExtensionPack.kt
@@ -1,11 +1,19 @@
 package ru.livetyping.extensionpack
 
-import android.content.Context
-import android.widget.Toast
+import kotlinx.coroutines.*
 
-object AndroidExtensionPack {
+fun CoroutineScope.launchUI(callback: suspend () -> Unit) = launch(Dispatchers.Main) { callback() }
 
-    fun makeToast(context: Context) {
-        Toast.makeText(context, "Greeting", Toast.LENGTH_SHORT).show()
-    }
-}
+fun CoroutineScope.launchUI(handler: CoroutineExceptionHandler, callback: suspend () -> Unit) =
+    launch(Dispatchers.Main + handler) { callback() }
+
+fun CoroutineScope.launchIO(callback: suspend () -> Unit) = launch(Dispatchers.IO) { callback() }
+
+fun CoroutineScope.launchIO(handler: CoroutineExceptionHandler, callback: suspend () -> Unit) =
+    launch(Dispatchers.IO + handler) { callback() }
+
+suspend fun <T> withIO(callback: suspend () -> T) = withContext(Dispatchers.IO) { callback() }
+
+suspend fun <T> withUI(callback: suspend () -> T) = withContext(Dispatchers.Main) { callback() }
+
+fun <T> CoroutineScope.asyncIO(callback: suspend () -> T) = async(Dispatchers.IO) { callback() }
